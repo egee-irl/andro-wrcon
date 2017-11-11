b995be813eeeab17ac2fabc21381f9fdaca108d5
egee-irl <brian@egee.io>
2017-11-11 07:07:50 -0800

    yeah whoops

--- transform.sh
+++ transform.sh
@@ -2,5 +2,5 @@
 
 sed -i s/_REDIS_/$REDIS/ dockerfile
 sed -i s/_RUSTIP_/$RUST_IP/ dockerfile
-sed -i s/_RUSTPORT_/$DISCORD_TOKEN/ dockerfile
+sed -i s/_RUSTPORT_/$RUST_PORT/ dockerfile
 sed -i s/_RUSTPASSWORD_/$RUST_PASSWORD/ dockerfile
