193ad2536976fb635f8cf4619f4660ac21eb9c7b
egee-irl <brian@egee.io>
2017-12-01 13:26:38 -0800

    get rid of it!

--- run.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-docker build --build-arg rust_ip=$RUST_IP --build-arg rust_port=$RUST_PORT --build-arg rust_password=$RUST_PASSWORD --build-arg discord_token=$DISCORD_TOKEN . -t androgee
-# docker run 
--- transform.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i s/_REDIS_/$REDIS/ dockerfile
-sed -i s/_RUSTIP_/$RUST_IP/ dockerfile
-sed -i s/_RUSTPORT_/$RUST_PORT/ dockerfile
-sed -i s/_RUSTPASSWORD_/$RUST_PASSWORD/ dockerfile
